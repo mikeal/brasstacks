00a1e5a8b71049f5a46b5c882d1927202c3619a9
Mikeal Rogers <mikeal.rogers@gmail.com>
2009-07-23 13:13:46 -0700

    Fixing db url.

--- wsgi_app.py
+++ wsgi_app.py
@@ -3,5 +3,5 @@ import couchquery
 from brasstacks import sitecompare
 
 application = brasstacks.application
-site_compare = sitecompare.SiteCompareApplication(couchquery.CouchDatabase("http://localhost:5984"))
+site_compare = sitecompare.SiteCompareApplication(couchquery.CouchDatabase("http://localhost:5984/sitecompare"))
 application.add_resource('sitecompare', site_compare)
