599a72653a23d49a6bf3eb42706832cad57620ba
Mikeal Rogers <mikeal.rogers@gmail.com>
2009-08-04 17:43:13 -0700

    Fixing wsgi app.

--- wsgi_app.py
+++ wsgi_app.py
@@ -1,9 +1,31 @@
-import brasstacks
+import webenv
+from webenv.rest import RestApplication
 import couchquery
+
+import brasstacks
 from brasstacks import sitecompare
 from brasstacks import users
-application = brasstacks.application
-site_compare = sitecompare.SiteCompareApplication(couchquery.CouchDatabase("http://localhost:5984/sitecompare"))
-application.add_resource('sitecompare', site_compare)
+from brasstacks import fennec
+
+
+class Stub(RestApplication):
+    def GET(self, request, *args):
+        return webenv.HtmlResponse('<html><head><title>Nope.</title></head><body>Nope.</body></html>')
+
+sitecompare_application = sitecompare.SiteCompareApplication(
+    couchquery.CouchDatabase("http://localhost:5984/sitecompare")
+    )
+db = couchquery.CouchDatabase("http://localhost:5984/brasstacks")
+db.sync_design_doc("sitecompare", sitecompare.design_doc)
+db.sync_design_doc("brasstacks", brasstacks.design_doc)
+db.sync_design_doc("fennecBrasstacks", fennec.design_doc)
 users_application = users.UsersApplication(db)
+fennec_application = fennec.FennecApplication(db)
+application = Stub()
+application.add_resource('sitecompare', sitecompare_application)
 application.add_resource('users', users_application)
+application.add_resource('fennec', fennec_application)
+
+
+
+
