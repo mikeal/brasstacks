e3ce5d08a40602f6ce0d41b330444ce62cf193b6
Mikeal Rogers <mikeal.rogers@gmail.com>
2009-09-10 16:17:33 -0700

    Making the command line interface much easier to use.

--- brasstacks/__init__.py
+++ brasstacks/__init__.py
@@ -11,88 +11,6 @@ import cronjob
 
 cron = cronjob.run
 
-# from mako.lookup import TemplateLookup
-# 
-this_dir = os.path.abspath(os.path.dirname(__file__))
-design_doc = os.path.join(this_dir, 'views')
-static_dir = os.path.join(this_dir, 'static')
-# 
-# lookup = TemplateLookup(directories=[os.path.join(this_dir, 'templates')])
-# 
-# def setup():
-#     from cherrypy import wsgiserver
-#     import pouch
-#     
-#     from brasstacks.rest import BrassTacksApplication
-#     pouch.set_globals('http://127.0.0.1:5984', 'brasstacks')
-#     pouch.sync_design_doc(os.path.join(this_dir, 'designDocs', 'testRuns'), 'testRuns')
-#     
-#     views = pouch.GLOBAL_DB.views
-#     
-#     application = BrassTacksApplication(views=views, debug=True)
-#     
-#     httpd = wsgiserver.CherryPyWSGIServer(('0.0.0.0', 8888,), 
-#                                           application, server_name='brasstacks-httpd',
-#                                           numthreads=50)
-#     return httpd
-# 
-# def cli():
-#     httpd = setup()
-#     
-#     print 'Serving on http://localhost:8888/'
-#     try:
-#         httpd.start()
-#     except KeyboardInterrupt:
-#         httpd.stop()
-
-
-def get_wsgi_server(db):
-    from logcompare import LogCompareApplication
-    from users import UsersApplication
-    from sitecompare import SiteCompareApplication
-    from tcm import TestCaseManagerApplication
-    from fennec import FennecApplication
-    from brasstacks.mozmill import MozmillApplication
-    a = Stub()
-    users_application = UsersApplication(db)
-    fennec_application = FennecApplication(db)
-    tcm_application = TestCaseManagerApplication(db)
-    logcompare_application = LogCompareApplication(db)
-    mozmill_application = MozmillApplication(db)
-    a.add_resource('sitecompare', SiteCompareApplication(db))
-    a.add_resource('users', users_application)
-    a.add_resource('fennec', fennec_application)
-    a.add_resource('tcm', tcm_application)
-    a.add_resource('static', FileServerApplication(static_dir))
-    a.add_resource('users', users_application)
-    a.add_resource('logcompare', logcompare_application)
-    a.add_resource('mozmill', mozmill_application)
-    httpd = make_server('', 8888, a)
-    return httpd    
-
-def cli():
-    db = [i for i in sys.argv if i.startswith('http')]
-    if len(db) is 1:
-        db = couchquery.Database(db[0], cache=Cache())
-    else:
-        db = couchquery.Database('http://localhost:5984/brasstacks')
-    import sitecompare
-    import fennec
-    import tcm
-    import brasstacks
-    import logcompare
-    sync(db)
-    httpd = get_wsgi_server(db)
-    print "Serving on http://localhost:8888/"
-    httpd.serve_forever()
-
-class Cache(dict):
-    def __init__(self, *args, **kwargs):
-        super(Cache, self).__init__(*args, **kwargs)
-        setattr(self, 'del', lambda *args, **kwargs: dict.__delitem__(*args, **kwargs) )
-    get = lambda *args, **kwargs: dict.__getitem__(*args, **kwargs)
-    set = lambda *args, **kwargs: dict.__setitem__(*args, **kwargs)
-    
 class Stub(RestApplication):
     def GET(self, request, favicon=None):
         if favicon:
@@ -103,36 +21,94 @@ class Stub(RestApplication):
         html += '</body></html>'
         return webenv.HtmlResponse(html)
 
-application = Stub()
+this_dir = os.path.abspath(os.path.dirname(__file__))
+design_doc = os.path.join(this_dir, 'views')
+static_dir = os.path.join(this_dir, 'static')
 
-def sync(db=None):
-    if db is None:
-        import sys
-        db = couchquery.Database(sys.argv[-1])
-    import sitecompare
-    import brasstacks
-    import fennec
-    import logcompare
-    import tcm
-    import crashtest
-    db.sync_design_doc("sitecompare", sitecompare.design_doc)
-    db.sync_design_doc("brasstacks", brasstacks.design_doc)
-    db.sync_design_doc("logcompare", logcompare.design_doc)
-    db.sync_design_doc("fennec", fennec.design_doc)
-    db.sync_design_doc("fennecFailures", fennec.failures_design_doc)
-    db.sync_design_doc("tcm", tcm.design_doc)
-    db.sync_design_doc("tcmTags", tcm.tags_design_doc)
-
-def crashtest_cli():
-    import crashtest
-    crashdb = couchquery.Database('http://localhost:5984/crashtest')
-    resultdb = couchquery.Database('http://localhost:5984/crashtest_results')
-    crashdb.sync_design_doc("crashes", crashtest.crashes_design_doc)
-    resultdb.sync_design_doc("jobs", crashtest.jobs_design_doc)
-    resultdb.sync_design_doc("results", crashtest.results_design_doc)
+def sync(db, names):
+    if 'sitecompare' in names:
+        from brasstacks import sitecompare
+        db.sync_design_doc("sitecompare", sitecompare.design_doc)
+    if 'logcompare' in names:
+        from brasstacks import logcompare
+        db.sync_design_doc("logcompare", logcompare.design_doc)
+    if 'fennec' in names:
+        from brasstacks import fennec
+        db.sync_design_doc("fennec", fennec.design_doc)
+        db.sync_design_doc("fennecFailures", fennec.failures_design_doc)
+    if 'users' in names or 'brasstacks' in names:
+        import brasstacks
+        db.sync_design_doc("brasstacks", brasstacks.design_doc)
+    if 'tcm' in names:
+        from brasstacks import tcm
+        db.sync_design_doc("tcm", tcm.design_doc)
+        db.sync_design_doc("tcmTags", tcm.tags_design_doc)
+    if 'crashtest' in names:
+        from brasstacks import crashtest
+        if 'crashes' in names:
+            db.sync_design_doc("crashes", crashtest.crashes_design_doc)
+        if 'crash_results' in names:
+            db.sync_design_doc("jobs", crashtest.jobs_design_doc)
+            db.sync_design_doc("results", crashtest.results_design_doc)
+    
+def get_application(db, names):
     a = Stub()
-    a.add_resource('crashtest', crashtest.CrashTestApplication(crashdb, resultdb))
+    a.add_resource('static', FileServerApplication(static_dir))
+    if 'sitecompare' in names:
+        from brasstacks.sitecompare import SiteCompareApplication
+        a.add_resource('sitecompare', SiteCompareApplication(db))
+    if 'logcompare' in names:
+        from brasstacks.logcompare import LogCompareApplication
+        logcompare_application = LogCompareApplication(db)
+        a.add_resource('logcompare', logcompare_application)
+    if 'users' in names or 'brasstacks' in names:
+        from brasstacks.users import UsersApplication
+        users_application = UsersApplication(db)
+        a.add_resource('users', users_application)
+    if 'tcm' in names:
+        from brasstacks.tcm import TestCaseManagerApplication
+        tcm_application = TestCaseManagerApplication(db)
+        a.add_resource('tcm', tcm_application)
+    if 'fennec' in names:
+        from brasstacks.fennec import FennecApplication
+        fennec_application = FennecApplication(db)
+        a.add_resource('fennec', fennec_application)
+    if 'mozmill' in names:
+        from brasstacks.mozmill import MozmillApplication
+        mozmill_application = MozmillApplication(db)
+        a.add_resource('mozmill', mozmill_application)
+    if 'crashtest' in names:
+        resultdb = couchquery.Database(db.uri[:-1]+'_results')
+        a.add_resource('crashtest', crashtest.CrashTestApplication(db, resultdb))
+    return a
+
+def cli():
+    db = [i for i in sys.argv if i.startswith('http')]
+    assert len(db) is 1
+    dbname = db[0]
+    db = couchquery.Database(dbname, cache=Cache())
+    sys.argv.remove(dbname)
+    
+    sync(db, sys.argv)
+    a = get_application(db, sys.argv)
     httpd = make_server('', 8888, a)
     print "Serving on http://localhost:8888/"
     httpd.serve_forever()
+
+def sync_cli(db=None):
+    db = [i for i in sys.argv if i.startswith('http')]
+    assert len(db) is 1
+    dbname = db[0]
+    db = couchquery.Database(dbname, cache=Cache())
+    sys.argv.remove(dbname)
+    
+    sync(db, sys.argv)
+
+class Cache(dict):
+    def __init__(self, *args, **kwargs):
+        super(Cache, self).__init__(*args, **kwargs)
+        setattr(self, 'del', lambda *args, **kwargs: dict.__delitem__(*args, **kwargs) )
+    get = lambda *args, **kwargs: dict.__getitem__(*args, **kwargs)
+    set = lambda *args, **kwargs: dict.__setitem__(*args, **kwargs)
+
     
