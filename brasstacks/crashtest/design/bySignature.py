f104f3438e971598705c668d0a707d6c989be3bf
Mikeal Rogers <mikeal.rogers@gmail.com>
2009-11-16 13:27:56 -0800

    Fixes to python view for crashtests.

--- brasstacks/crashtest/design/bySignature.py
+++ brasstacks/crashtest/design/bySignature.py
@@ -1,18 +1,12 @@
 @map_function
 def signature_map(doc):
     if 'signature' in doc and doc['signature']:
-        emit(doc['signature'], [doc['url'], doc['os_version']])
+        emit([doc['signature'], doc['os_name'], doc['version'][:3]], doc['url'])
 
 @reduce_function
-def red(keys, values, length):
-    x = {}
-    for i in range(length):
-        x.setdefault(keys[i], []).append(values[i])
-    return x
+def red(values):
+    return values
 
 @rereduce_function
 def rered(values):
-    res = {}
-    for x in values:
-        res.update(x)
-    return res
+    return [i for i in [v for v in values]]
